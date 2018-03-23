dc04216fd602a9d3980ffc27ca6d992c78ef755f
Zachary Dudley Friedman <ZacharyDFriedman@gmail.com>
2018-03-23 16:51:26 -0500

    added tests and fixed function

--- utility.js
+++ utility.js
@@ -1,21 +1,13 @@
 const utility = (number) => {
-  const whole = Math.trunc(number)
-  let decimal = number - whole
-  let truncDecimal = Math.trunc(decimal * 10000) / 10000
+  let splitNum = number.toString().split('.')
 
-  return (whole + truncDecimal).toString()
+  if (splitNum.length === 1) {
+    return splitNum[0].toString()
+  } else {
+    let decimal = splitNum[1].slice(0, 4) || 0
+
+    return [splitNum[0], '.', decimal].join('')
+  }
 }
-
-// const utilityTypeCheck = (number) => {
-//   if (typeof number === 'number') {
-//     const whole = Math.trunc(number)
-//     let decimal = number - whole
-//     let truncDecimal = Math.trunc(decimal * 10000) / 10000
-
-//     return (whole + truncDecimal).toString()
-//   } else {
-//     throw new Error('Argument must be number')
-//   }
-// }
 
 module.exports = utility
--- utility.test.js
+++ utility.test.js
@@ -18,4 +18,12 @@ describe('utility function', () => {
   test('does not round', () => {
     expect(utility(75.44519939334)).not.toBe('75.4452')
   })
+
+  test('pass these examples', () => {
+    expect(utility(15.0)).toBe('15')
+    expect(utility(75.44510939334)).toBe('75.4451')
+    expect(utility(75.4450)).toBe('75.445')
+    expect(utility(6)).toBe('6')
+    expect(utility(1.5)).toBe('1.5')
+  })
 })
